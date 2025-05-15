02303a8902b3e5ac0d0647b02c607810aab79ddd
isinning1 <i.sinning@uniandes.edu.co>
2025-05-15 14:40:55 -0500

    AJustes de archivos de configuracion

---
+++ src/libro/libro.dto.ts
@@ -0,0 +1,20 @@
+// src/libro/libro.dto.ts
+// src/libro/libro.dto.ts
+import { IsString, IsDateString, Length } from 'class-validator';
+
+export class LibroDto {
+  @IsString()
+  @Length(1, 100)
+  titulo: string;
+
+  @IsString()
+  @Length(1, 100)
+  autor: string;
+
+  @IsDateString()
+  fechaPublicacion: string;
+
+  @IsString()
+  @Length(10, 20)
+  isbn: string;
+}
